41a83a6b33b6def50fea92f24585a1a2f7958983
Max Metral <max.metral@sesamecare.com>
2023-08-04 15:18:46 -0400

    fix(types): rework the query and path types
    BREAKING CHANGE: the query and path types now separate the paths
    argument and the actual path, and define individual types for each
    HTTP method

--- src/type-utils.ts
+++ src/type-utils.ts
@@ -8,14 +8,28 @@ export type JsonPostBody<T extends { post: unknown }> = JsonRequestBody<T, 'post
 export type JsonPutBody<T extends { put: unknown }> = JsonRequestBody<T, 'put'>;
 export type JsonPatchBody<T extends { patch: unknown }> = JsonRequestBody<T, 'patch'>;
 
-export type QueryParams<T extends { get?: unknown }, M extends keyof T = 'get'> = T[M] extends {
-  parameters: { query: infer U };
+type HttpMethod = 'get' | 'delete' | 'post' | 'put' | 'patch';
+
+export type MethodQueryParams<T, P extends keyof T, M extends HttpMethod> = T[P] extends {
+  [key in M]: { parameters: { query: infer U } };
 }
   ? U
   : never;
 
-export type PathParams<T extends { get?: unknown }, M extends keyof T> = T[M] extends {
-  parameters: { path: infer U };
+export type GetQueryParams<T, P extends keyof T> = MethodQueryParams<T, P, 'get'>;
+export type DeleteQueryParams<T, P extends keyof T> = MethodQueryParams<T, P, 'delete'>;
+export type PostQueryParams<T, P extends keyof T> = MethodQueryParams<T, P, 'post'>;
+export type PutQueryParams<T, P extends keyof T> = MethodQueryParams<T, P, 'put'>;
+export type PatchQueryParams<T, P extends keyof T> = MethodQueryParams<T, P, 'patch'>;
+
+export type PathParams<T, P extends keyof T, M extends HttpMethod> = T[P] extends {
+  [key in M]: { parameters: { path: infer U } };
 }
   ? U
   : never;
+
+export type GetPathParams<T, P extends keyof T> = PathParams<T, P, 'get'>;
+export type DeletePathParams<T, P extends keyof T> = PathParams<T, P, 'delete'>;
+export type PostPathParams<T, P extends keyof T> = PathParams<T, P, 'post'>;
+export type PutPathParams<T, P extends keyof T> = PathParams<T, P, 'put'>;
+export type PatchPathParams<T, P extends keyof T> = PathParams<T, P, 'patch'>;
