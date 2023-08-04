ab2fa38baf9a31726db497f4d149421a8e0dedf7
Max Metral <max.metral@sesamecare.com>
2023-08-04 11:29:35 -0400

    fix(util): add a utility type for the query args

--- src/type-utils.ts
+++ src/type-utils.ts
@@ -7,3 +7,15 @@ export type JsonRequestBody<T, M extends keyof T> = T[M] extends {
 export type JsonPostBody<T extends { post: unknown }> = JsonRequestBody<T, 'post'>;
 export type JsonPutBody<T extends { put: unknown }> = JsonRequestBody<T, 'put'>;
 export type JsonPatchBody<T extends { patch: unknown }> = JsonRequestBody<T, 'patch'>;
+
+export type QueryParams<T extends { get?: unknown }, M extends keyof T = 'get'> = T[M] extends {
+  parameters: { query: infer U };
+}
+  ? U
+  : never;
+
+export type PathParams<T extends { get?: unknown }, M extends keyof T> = T[M] extends {
+  parameters: { path: infer U };
+}
+  ? U
+  : never;
